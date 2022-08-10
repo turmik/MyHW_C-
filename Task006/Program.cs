0dc8246db398d26c3e02c55ed5402a169f6f2815
Mikhael <turmik71@gmail.com>
2022-08-10 11:26:13 +0200

    Решили Task006

--- Task006/Program.cs
+++ Task006/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+int Number = int.Parse(Console.ReadLine());
+
+if ( Number % 2 == 0)
+   Console.WriteLine("да");
+
+else
+    Console.WriteLine("нет");
