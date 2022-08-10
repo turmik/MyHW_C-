27b8caeff4c29f2ca38daec85d3b80c016be532f
Mikhael <turmik71@gmail.com>
2022-08-10 18:08:39 +0200

    Создал промежуточный вариант решения задачи

--- Task008/Program.cs
+++ Task008/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("008Hello, World!");
+﻿Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+for ( ; num > 1 ; num = num - 1);
+   {
+       if (num % 2 == 0);
+    Console.WriteLine($"{num}");
+   }
+
+
+
