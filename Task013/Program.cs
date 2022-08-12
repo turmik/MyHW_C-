288d155c0f6c1bbe9edc114ac45f23c7c3582b6f
Mikhael <turmik71@gmail.com>
2022-08-12 19:17:54 +0200

    Решение Task013

--- Task013/Program.cs
+++ Task013/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿Console.Write("Введите число");
+int a = int.Parse(Console.ReadLine());
+int a1 = a % 1000;
+int a2 = a1 / 100;
+
+
+if (a < 100) 
+   Console.WriteLine("Третьей цифры нет");
+
+else 
+    Console.WriteLine($"{a2}");
+
+
+
