f3402a09ee8f29147809c0a0cbaf5f538d236856
Mikhael <turmik71@gmail.com>
2022-08-16 11:00:35 +0200

    Задача004 решена

--- Task004/Program.cs
+++ Task004/Program.cs
@@ -1,4 +1,10 @@
-﻿Console.Write("ВВедите число A: ");
+﻿/*Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22*/
+
+Console.Write("ВВедите число A: ");
 int NumberA = int.Parse(Console.ReadLine());
 Console.Write("Введите число B: ");
 int NumberB = int.Parse(Console.ReadLine());
